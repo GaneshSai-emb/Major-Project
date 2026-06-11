83834efa2fcd60858ac48d8fcac73187b6e1a353
GaneshSai-emb <ganeshsai.emb@gmail.com>
2026-06-11 15:17:55 +0530

    Update kpm_defines.h

--- kpm_defines.h
+++ kpm_defines.h
@@ -1,9 +1,11 @@
-#define ROW0 16
-#define ROW1 17
-#define ROW2 18
-#define ROW3 19
+// Row pin definitions for 4x4 keypad matrix
+#define ROW0 16   // Row 0 connected to GPIO pin 16
+#define ROW1 17   // Row 1 connected to GPIO pin 17
+#define ROW2 18   // Row 2 connected to GPIO pin 18
+#define ROW3 19   // Row 3 connected to GPIO pin 19
 
-#define COL0 20
-#define COL1 21
-#define COL2 22
-#define COL3 23
+// Column pin definitions for 4x4 keypad matrix
+#define COL0 20   // Column 0 connected to GPIO pin 20
+#define COL1 21   // Column 1 connected to GPIO pin 21
+#define COL2 22   // Column 2 connected to GPIO pin 22
+#define COL3 23   // Column 3 connected to GPIO pin 23
