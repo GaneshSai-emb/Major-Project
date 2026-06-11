e30b2b2eda0814e6f0b0d73c48b3ae4c0c2a6d5e
GaneshSai-emb <ganeshsai.emb@gmail.com>
2026-06-11 15:03:13 +0530

    Update kpm.h

--- kpm.h
+++ kpm.h
@@ -1,9 +1,21 @@
-#include"types.h"
+/* KPM function declarations */
 
+#include "types.h"
+
+/* Initialize keypad */
 void InitKPM(void);
-u8 colscan(void);
-u8 RowCheck(void);
-u8 ColCheck(void);
-u8 KeyScan(void);
-u32 ReadNum(void); 
 
+/* Scan columns for key press */
+u8 ColScan(void);
+
+/* Find pressed row */
+u8 RowCheck(void);
+
+/* Find pressed column */
+u8 ColCheck(void);
+
+/* Scan keypad and return key value */
+u8 KeyScan(void);
+
+/* Read number from keypad */
+u32 ReadNum(void);
